6de6d9c18b226a77860cc8576984cf40b39b99ab
Fredrik Johansen <fredrik1206@gmail.com>
2024-01-02 14:15:15 +0100

    chore(database): adds password and salt to users table

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
   id varchar(255) NOT NULL UNIQUE PRIMARY KEY,
   username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  salt varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
   birth_date date NOT NULL
 );
