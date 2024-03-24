e399efb99e889649fd9fe1cc700b9861c5f7af50
Fredrik Johansen <fredrik1206@gmail.com>
2024-03-24 11:17:10 +0100

    feat(helpers): adds helper function to sign JWTs

---
+++ src/helpers/signJwt.ts
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import { config } from "../config";
+
+interface SignJwtOptions {
+  userId: string;
+  username: string;
+}
+
+export const signJwt = ({ userId, username }: SignJwtOptions) => {
+  return jwt.sign(
+    {
+      userId,
+      username,
+    },
+    config.jwtPrivateKey,
+    {
+      expiresIn: "24h",
+    }
+  );
+};
