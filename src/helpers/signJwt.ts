2ec34f9e876a9b83ee5dda63ae9abcf7661885ad
Fredrik Johansen <fredrik1206@gmail.com>
2024-03-29 12:14:47 +0100

    feat(helpers): adds proper error handling for signJwt function

--- src/helpers/signJwt.ts
+++ src/helpers/signJwt.ts
@@ -1,20 +1,31 @@
 import jwt from "jsonwebtoken";
 import { config } from "../config";
+import { InternalServerError } from "../errors/server";
+import { logger } from "../logging";
 
 interface SignJwtOptions {
-  userId: string;
-  username: string;
+  payload: Record<string, unknown>;
+  expiresInSeconds: number;
+  tokenId?: string;
 }
 
-export const signJwt = ({ userId, username }: SignJwtOptions) => {
-  return jwt.sign(
-    {
-      userId,
-      username,
-    },
-    config.jwtPrivateKey,
-    {
-      expiresIn: "24h",
-    },
-  );
+export const signJwt = ({ payload, expiresInSeconds, tokenId }: SignJwtOptions): string => {
+  try {
+    if (tokenId) {
+      return jwt.sign(payload, config.jwtPrivateKey, {
+        expiresIn: expiresInSeconds,
+        jwtid: tokenId,
+      });
+    }
+
+    return jwt.sign(payload, config.jwtPrivateKey, {
+      expiresIn: expiresInSeconds,
+    });
+  } catch (error) {
+    logger.error({ error }, "Failed signing JWT token");
+    throw new InternalServerError({
+      code: "error-signing-jwt-token",
+      message: "Unknown error while signing JWT token",
+    });
+  }
 };
