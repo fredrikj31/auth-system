c11658f01611a3acba91276ebc6b449f8512b51b
Fredrik Johansen <fredrik1206@gmail.com>
2024-07-28 12:22:18 +0200

    feat(app): adds design for login page

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,3 +1,30 @@
+import { Button } from "@shadcn-ui/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@shadcn-ui/components/ui/card";
+import { Input } from "@shadcn-ui/components/ui/input";
+import { Label } from "@shadcn-ui/components/ui/label";
+
 export const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>Enter your email below to login to your account.</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="johndoe@mail.com" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Sign in</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
