487056ec79eb0ca5bec803ff31c9d864351af72d
Fredrik Johansen <fredrik1206@gmail.com>
2024-07-28 14:03:50 +0200

    feat(app): adds home page designs

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,3 +1,38 @@
+import { Button } from "@shadcn-ui/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@shadcn-ui/components/ui/card";
+
 export const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl">Welcome JohnDoe</CardTitle>
+          <CardDescription>Here is a list of all your information.</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-row justify-between">
+              <span className="text-zinc-500 font-semibold">Id:</span>
+              <span className="text-zinc-700">29421b51-3960-4ba0-bfa0-d51020a108be</span>
+            </div>
+            <div className="flex flex-row justify-between">
+              <span className="text-zinc-500 font-semibold">Email:</span>
+              <span className="text-zinc-700">johndoe@mail.com</span>
+            </div>
+            <div className="flex flex-row justify-between">
+              <span className="text-zinc-500 font-semibold">Username:</span>
+              <span className="text-zinc-700">JohnDoe</span>
+            </div>
+            <div className="flex flex-row justify-between">
+              <span className="text-zinc-500 font-semibold">Birthday:</span>
+              <span className="text-zinc-700">17/05/2001</span>
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Logout</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
