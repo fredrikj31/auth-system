a415fc4e7c29b59f4771f668d719b9e773a490e2
Fredrik Johansen <fredrik1206@gmail.com>
2024-08-13 20:33:03 +0200

    chore(npm): installs @tanstack/react-query and devtools

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,8 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { HomePage } from "./pages/Home.tsx";
 import { LoginPage } from "./pages/Login.tsx";
 import { SignupPage } from "./pages/Signup.tsx";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@shadcn-ui/components/ui/toaster.tsx";
 
 const queryClient = new QueryClient();
@@ -26,6 +27,7 @@ export const App = () => {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools client={queryClient} initialIsOpen={false} />
         <RouterProvider router={router} />
         <Toaster />
       </QueryClientProvider>
