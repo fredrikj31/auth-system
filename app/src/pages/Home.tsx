d0a4aac74849f705d599a5579c158f74c4b36a94
Fredrik Johansen <fredrik1206@gmail.com>
2024-08-13 20:39:26 +0200

    feat(app): adds pages design and logic to signup, login & logout

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,36 +1,93 @@
 import { Button } from "@shadcn-ui/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@shadcn-ui/components/ui/card";
+import { Skeleton } from "@shadcn-ui/components/ui/skeleton";
+import { useGetUser } from "../api/getUser/useGetUser";
+import { useLogoutUser } from "../api/logoutUser/useLogoutUser";
+import cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+
+  const { data, isFetching, isError } = useGetUser();
+  const { mutate: logoutUser } = useLogoutUser();
+
+  const logout = () => {
+    logoutUser(undefined, {
+      onSuccess: () => {
+        cookies.remove("access_token");
+        cookies.remove("refresh_token");
+        navigate("/login");
+      },
+    });
+  };
+
+  if (isFetching) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <Card className="w-full max-w-md">
+          <CardHeader>
+            <Skeleton className="w-full h-14" />
+          </CardHeader>
+          <CardContent className="flex flex-col gap-2">
+            <Skeleton className="w-full h-6" />
+            <Skeleton className="w-full h-6" />
+            <Skeleton className="w-full h-6" />
+            <Skeleton className="w-full h-6" />
+          </CardContent>
+          <CardFooter>
+            <Skeleton className="w-full h-10" />
+          </CardFooter>
+        </Card>
+      </div>
+    );
+  }
+
+  if (!data || isError) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <Card className="w-full max-w-md">
+          <CardHeader>
+            <CardTitle className="text-2xl">Profile not found.</CardTitle>
+          </CardHeader>
+          <CardContent>Your profile information was not found. Please contact an admin.</CardContent>
+          <CardFooter>
+            <Button className="w-full">Logout</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen flex justify-center items-center">
       <Card className="w-full max-w-md">
         <CardHeader>
-          <CardTitle className="text-2xl">Welcome JohnDoe</CardTitle>
+          <CardTitle className="text-2xl">Welcome {data.username}</CardTitle>
           <CardDescription>Here is a list of all your information.</CardDescription>
         </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-row justify-between">
-              <span className="text-zinc-500 font-semibold">Id:</span>
-              <span className="text-zinc-700">29421b51-3960-4ba0-bfa0-d51020a108be</span>
-            </div>
-            <div className="flex flex-row justify-between">
-              <span className="text-zinc-500 font-semibold">Email:</span>
-              <span className="text-zinc-700">johndoe@mail.com</span>
-            </div>
-            <div className="flex flex-row justify-between">
-              <span className="text-zinc-500 font-semibold">Username:</span>
-              <span className="text-zinc-700">JohnDoe</span>
-            </div>
-            <div className="flex flex-row justify-between">
-              <span className="text-zinc-500 font-semibold">Birthday:</span>
-              <span className="text-zinc-700">17/05/2001</span>
-            </div>
+        <CardContent className="flex flex-col gap-2">
+          <div className="flex flex-row justify-between">
+            <span className="text-zinc-500 font-semibold">Id:</span>
+            <span className="text-zinc-700">{data.id}</span>
+          </div>
+          <div className="flex flex-row justify-between">
+            <span className="text-zinc-500 font-semibold">Email:</span>
+            <span className="text-zinc-700">{data.email}</span>
+          </div>
+          <div className="flex flex-row justify-between">
+            <span className="text-zinc-500 font-semibold">Username:</span>
+            <span className="text-zinc-700">{data.username}</span>
+          </div>
+          <div className="flex flex-row justify-between">
+            <span className="text-zinc-500 font-semibold">Birthday:</span>
+            <span className="text-zinc-700">{data.birthDate}</span>
           </div>
         </CardContent>
         <CardFooter>
-          <Button className="w-full">Logout</Button>
+          <Button className="w-full" onClick={logout}>
+            Logout
+          </Button>
         </CardFooter>
       </Card>
     </div>
--- app/src/pages/Signup.tsx
+++ app/src/pages/Signup.tsx
@@ -2,16 +2,66 @@ import { Label } from "@radix-ui/react-label";
 import { Button } from "@shadcn-ui/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@shadcn-ui/components/ui/card";
 import { Input } from "@shadcn-ui/components/ui/input";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Popover, PopoverContent, PopoverTrigger } from "@shadcn-ui/components/ui/popover";
 import { cn } from "@shadcn-ui/lib/utils";
 import { CalendarIcon } from "lucide-react";
 import { Calendar } from "@shadcn-ui/components/ui/calendar";
 import { useState } from "react";
 import { format } from "date-fns";
+import { useSignupUser } from "../api/signupUser/useSignupUser";
+import { useToast } from "@shadcn-ui/components/ui/use-toast";
 
 export const SignupPage = () => {
-  const [date, setDate] = useState<Date>();
+  const [email, setEmail] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [birthDate, setBirthDate] = useState<Date>();
+
+  const { toast } = useToast();
+  const navigate = useNavigate();
+  const { mutate: signupUser } = useSignupUser();
+
+  const signup = () => {
+    signupUser(
+      { email, username, password, birthDate: format(birthDate ?? new Date(), "yyyy-MM-dd") },
+      {
+        onSuccess: () => {
+          toast({
+            title: "Signup successfully!",
+            duration: 2000,
+          });
+          navigate("/login");
+        },
+        onError: (error) => {
+          switch (error.statusCode) {
+            case 404:
+              toast({
+                title: "User not found.",
+                description: "A user with that username was not found.",
+                duration: 5000,
+              });
+              break;
+            case 409:
+              toast({
+                title: "Username is taken.",
+                description: "That username is already taken by another user.",
+                duration: 5000,
+              });
+              break;
+            default:
+              toast({
+                variant: "destructive",
+                title: "Unknown Error",
+                description: "Unknown error while trying to sign up, please contact an admin.",
+                duration: 5000,
+              });
+              break;
+          }
+        },
+      },
+    );
+  };
 
   return (
     <div className="min-h-screen flex justify-center items-center">
@@ -24,36 +74,48 @@ export const SignupPage = () => {
           <div className="grid gap-4">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="johndoe@mail.com" required />
+              <Input
+                id="email"
+                type="email"
+                placeholder="johndoe@mail.com"
+                required
+                onChange={(e) => setEmail(e.currentTarget.value)}
+              />
             </div>
             <div className="flex flex-row gap-2">
               <div className="grid gap-2 min-w-52 w-full">
                 <Label htmlFor="username">Username</Label>
-                <Input id="username" type="text" placeholder="JohnDoe" required />
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="JohnDoe"
+                  required
+                  onChange={(e) => setUsername(e.currentTarget.value)}
+                />
               </div>
               <div className="grid gap-2 min-w-52 w-full">
-                <Label>Birthday</Label>
+                <Label>Birth Date</Label>
                 <Popover>
                   <PopoverTrigger asChild>
                     <Button
                       variant={"outline"}
-                      className={cn("justify-start text-left font-normal", !date && "text-muted-foreground")}
+                      className={cn("justify-start text-left font-normal", !birthDate && "text-muted-foreground")}
                     >
                       <CalendarIcon className="mr-2 h-4 w-4" />
-                      {date ? format(date, "PPP") : <span>Pick a date</span>}
+                      {birthDate ? format(birthDate, "PPP") : <span>Pick a date</span>}
                     </Button>
                   </PopoverTrigger>
                   <PopoverContent className="w-auto p-0">
-                    <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+                    <Calendar mode="single" selected={birthDate} onSelect={setBirthDate} initialFocus />
                   </PopoverContent>
                 </Popover>
               </div>
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
+              <Input id="password" type="password" onChange={(e) => setPassword(e.currentTarget.value)} />
             </div>
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full" onClick={signup}>
               Create an account
             </Button>
           </div>
