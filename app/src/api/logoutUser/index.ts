4f6eccf8ca66be7367d6030f8c7999bfe80f7b35
Fredrik Johansen <fredrik1206@gmail.com>
2024-10-14 21:37:55 +0200

    feat(app): adds refresh token to payload to logoutUser endpoint

--- app/src/api/logoutUser/index.ts
+++ app/src/api/logoutUser/index.ts
@@ -1,9 +1,13 @@
+import cookies from "js-cookie";
 import { apiClient } from "../client";
 import { ApiError } from "../errors";
 
 export const logoutUser = async (): Promise<void> => {
+  const refreshToken = cookies.get("refresh_token");
   try {
-    await apiClient.post("/logout");
+    await apiClient.post("/logout", {
+      refreshToken,
+    });
   } catch (error) {
     throw new ApiError({
       statusCode: 500,
